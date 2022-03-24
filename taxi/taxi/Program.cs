688c5b31eb10842d173b660786dc9b15d9f2c74c
Aina Nabilla <aina.saffira@students.amikom.ac.id>
2022-03-24 07:20:57 +0700

    Taxi
    Taxi object

---
+++ taxi/taxi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Index
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat Objek taxi
+            Taxi taxi = new Taxi();
+
+            // pengesetan nilai properties
+            taxi.DriverName = "Jono";
+            taxi.onDuty = true;
+            taxi.NumPassenger = 10;
+
+            // pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
